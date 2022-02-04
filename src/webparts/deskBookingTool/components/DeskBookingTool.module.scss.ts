5cadb2ccd889cbbf8ef6ec3ef2cc797bd57a0fe9
kalyan610 <89636024+kalyan610@users.noreply.github.com>
2022-02-04 16:13:58 +0530

    Add files via upload

---
+++ src/webparts/deskBookingTool/components/DeskBookingTool.module.scss.ts
@@ -0,0 +1,48 @@
+/* tslint:disable */
+require("./DeskBookingTool.module.css");
+const styles = {
+  button: 'button_2944c617',
+  label: 'label_2944c617',
+  header: 'header_2944c617',
+  col1: 'col1_2944c617',
+  Serachtextbox: 'Serachtextbox_2944c617',
+  boxsize: 'boxsize_2944c617',
+  categorysize: 'categorysize_2944c617',
+  DetAppName: 'DetAppName_2944c617',
+  msTeams: 'msTeams_2944c617',
+  detButton: 'detButton_2944c617',
+  ProceedButton: 'ProceedButton_2944c617',
+  coststyle: 'coststyle_2944c617',
+  myBorder: 'myBorder_2944c617',
+  commonstyle: 'commonstyle_2944c617',
+  commonstyleDescRightbox: 'commonstyleDescRightbox_2944c617',
+  borfont: 'borfont_2944c617',
+  myBackcolor: 'myBackcolor_2944c617',
+  whitDescBox: 'whitDescBox_2944c617',
+  myDescBox: 'myDescBox_2944c617',
+  myDescRightBox: 'myDescRightBox_2944c617',
+  onlyFont: 'onlyFont_2944c617',
+  alignCenter: 'alignCenter_2944c617',
+  AlignChange: 'AlignChange_2944c617',
+  commonstyleDescRightboxonly: 'commonstyleDescRightboxonly_2944c617',
+  dateTimeClass: 'dateTimeClass_2944c617',
+  tableborder: 'tableborder_2944c617',
+  tablecell: 'tablecell_2944c617',
+  tablecelldata: 'tablecelldata_2944c617',
+  tablecelldata1: 'tablecelldata1_2944c617',
+  smallBox: 'smallBox_2944c617',
+  greenBox: 'greenBox_2944c617',
+  redClass: 'redClass_2944c617',
+  normalClass: 'normalClass_2944c617',
+  Iconbutton: 'Iconbutton_2944c617',
+  chkboxDeactive: 'chkboxDeactive_2944c617',
+  chkboxActive: 'chkboxActive_2944c617',
+  resevedSeats1: 'resevedSeats1_2944c617',
+  resevedSeats: 'resevedSeats_2944c617',
+  emptySeats: 'emptySeats_2944c617',
+  headings: 'headings_2944c617',
+  MyStyling: 'MyStyling_2944c617'
+};
+
+export default styles;
+/* tslint:enable */
